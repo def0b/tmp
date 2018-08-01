1b07b8595bcff5bd91473a297a9768e64175a265
Пилипенко Роман Николаевич <pilipenko@fire.iac.spb.ru>
2018-08-01 13:04:34 +0300

    added main java

---
+++ Main.java
@@ -0,0 +1,15 @@
+ public static void main(String[] args) {
+
+//        System.out.println(sumNumbers("aa11b33"));
+//        System.out.println(scoresIncreasing([1, 1, 4);
+        //System.out.println(commonTwo(new String[]{"a", "b"}, new String[]{"a", "b"}));
+      //  System.out.println(commonTwo(new String[]{"a", "c", "x"}, new String[]{"b", "c", "d", "x"}));
+     //   System.out.println(bigHeights(new int[]{5, 13, 6, 7, 2}, 0, 3));
+        System.out.println(bigHeights(new int[]{5, 3, 6, 7, 2}, 2, 4));
+        //      System.out.println(scoresAverage((new int[]{2, 2, 4, 4})));
+
+//        System.out.println(gHappy("ggasdagg"));
+        //  System.out.println(maxBlock("abbCCCddBBBxx"));
+
+
+    }
